99d55861ff86235bd6bab530fedd3ac5005dab50
MACBOOK PRO RETINA <macbookproretina@MACBOOKs-MacBook-Pro-6.local>
2018-10-21 14:07:15 +0700

    Initial commit

---
+++ app/src/main/java/com/ziadsyahrul/footballmania/DetailClubActivity.java
@@ -0,0 +1,35 @@
+package com.ziadsyahrul.footballmania;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class DetailClubActivity extends AppCompatActivity {
+
+    @BindView(R.id.imgLogo)
+    ImageView imgLogo;
+    @BindView(R.id.txtTitle)
+    TextView txtTitle;
+    @BindView(R.id.txtDetail)
+    TextView txtDetail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_club);
+        ButterKnife.bind(this);
+
+        //TODO Mengubah textView dan imageView dengan data dari halaman sebelumnya
+        txtTitle.setText(getIntent().getStringExtra("nc"));
+        txtDetail.setText(getIntent().getStringExtra("dc"));
+        Glide.with(this).load(getIntent().getIntExtra("gc",0)).into(imgLogo);
+    }
+}
+
+
